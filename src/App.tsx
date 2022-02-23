a4d98e7d1b1d1836f75cf5171cfaa4f903ac5db4
imhilla <hillaryodhiambo282@gmail.com>
2022-02-23 05:41:08 +0300

    call functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 import "./App.css";
 import List from "./components/List";
 
-interface ISstate {
+export interface ISstate {
   people: {
     name: string;
     age: number;
-    url: string;
+    img: string;
     note?: string;
   }[];
 }
 function App() {
-  const [people, setPeople] = useState<ISstate["people"]>([]);
+  const [people, setPeople] = useState<ISstate["people"]>([
+    {
+      name: "LeBron James",
+      age: 35,
+      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg",
+    },
+  ]);
 
   return (
     <div className="App">
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { ISstate as Props } from "../App";
 interface IPrope {
-  people: {
-    name: string;
-    age: number;
-    url: string;
-    note?: string;
-  }[];
+  people: Props["people"];
 }
 
 const List: React.FC<IPrope> = ({ people }) => {
-  return <div>List</div>;
+  const renderList = (): JSX.Element[] => {
+    return people.map((person) => {
+      console.log(person);
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.img} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
 };
+
 export default List;
